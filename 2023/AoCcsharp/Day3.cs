6b3091ebb99caf95dd18a184ec6f605021f40699
Andy Raddatz <andyraddatz@gmail.com>
2023-12-06 21:19:55 -0600

    tweaks

---
+++ 2023/AoCcsharp/Day3.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+
+namespace AoCcsharp;
+public static class Day3
+{
+    public static int Part1()
+    {
+        // var input = File.ReadAllLines("data/day3.txt");
+        var sum = 0;
+
+        // TODO:
+
+        return sum;
+    }
+}
--- 2023/AoCcsharp/Program.cs
+++ 2023/AoCcsharp/Program.cs
@@ -1,9 +1,18 @@
 ﻿using AoCcsharp;
 
 var _stopWatch = new System.Diagnostics.Stopwatch();
+var _overallStopWatch = new System.Diagnostics.Stopwatch();
+_overallStopWatch.Start();
 _stopWatch.Start();
 Console.WriteLine($"Day 1 - Part 1: {Day1.Part1()} --- {_stopWatch.ElapsedMilliseconds} ms");
+_stopWatch.Restart();
 Console.WriteLine($"Day 1 - Part 2: {Day1.Part2()} --- {_stopWatch.ElapsedMilliseconds} ms");
+_stopWatch.Restart();
 Console.WriteLine($"Day 2 - Part 1: {Day2.Part1()} --- {_stopWatch.ElapsedMilliseconds} ms");
+_stopWatch.Restart();
 Console.WriteLine($"Day 2 - Part 2: {Day2.Part2()} --- {_stopWatch.ElapsedMilliseconds} ms");
+_stopWatch.Restart();
+Console.WriteLine($"Day 3 - Part 1: {Day3.Part1()} --- {_stopWatch.ElapsedMilliseconds} ms");
 _stopWatch.Stop();
+Console.WriteLine($"Total time: {_overallStopWatch.ElapsedMilliseconds} ms");
+_overallStopWatch.Stop();
